02123c02f7d86093bb81ac7c0bd56f93b05a94dc
Romulo <romuloctba@gmail.com>
2015-05-24 15:26:01 -0300

    broadcasting events

--- src/js/main.js
+++ src/js/main.js
@@ -1,1 +1,15 @@
-angular.module('rcTomatoe', []);
+angular.module('rcTomatoe', [])
+.controller('MainCtrl', function($rootScope, $scope, $timeout){
+	$scope.startTimer = function(){
+		$rootScope.$broadcast('timer-started')
+	}
+	$scope.pauseTimer = function(){
+		$rootScope.$broadcast('timer-paused')
+	}
+	$scope.resumeTimer = function(){
+		$rootScope.$broadcast('timer-resumed')
+	}
+	$scope.endTimer = function(){
+		$rootScope.$broadcast('timer-ended')
+	}
+});
